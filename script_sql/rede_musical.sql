0017dd4daf5457aa209d9f53bfc55bb8ba9e4d7f
ViCesar18 <viniciuscesar_cesar@hotmail.com>
2020-09-10 16:35:36 -0300

    Adicionei o script do banco em postgresSQL.

---
+++ script_sql/rede_musical.sql
@@ -0,0 +1,172 @@
+CREATE SCHEMA rede_musical;
+
+CREATE TABLE rede_musical.usuario (
+	username VARCHAR(12),
+	email VARCHAR(50) UNIQUE,
+	senha TEXT,
+	pnome VARCHAR(30),
+	snome VARCHAR(30),
+	dt_nascimento DATE,
+	cidade VARCHAR(50),
+	estado VARCHAR(50),
+	pais VARCHAR(50),
+	banda_favorita VARCHAR(20),
+	musica_favorita VARCHAR(20),
+	genero_favorito VARCHAR(20),
+	instrumento_favorito VARCHAR(20),
+	CONSTRAINT pk_usuario PRIMARY KEY(username)
+);
+
+CREATE TABLE rede_musical.usuario_instrumentos (
+	usuario_username VARCHAR(12),
+	instrumento VARCHAR(20),
+	CONSTRAINT pk_usuario_instrumentos PRIMARY KEY(usuario_username, instrumento),
+	CONSTRAINT fk_usuario_instrumentos FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.usuario_segue_usuario (
+	usuario_username VARCHAR(12),
+	usuario_username_seguido VARCHAR(12),
+	CONSTRAINT pk_usuario_segue_usuario PRIMARY KEY(usuario_username, usuario_username_seguido),
+	CONSTRAINT fk_usuario_seguindo FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_usuario_seguido FOREIGN KEY(usuario_username_seguido)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.post (
+	id SERIAL,
+	texto_post TEXT,
+	imagem TEXT,
+	dt_publicacao TIMESTAMP,
+	n_likes INT,
+	n_comentarios INT,
+	n_compartilhamentos INT,
+	usuario_username VARCHAR(12) NOT NULL,
+	CONSTRAINT pk_post PRIMARY KEY(id),
+	CONSTRAINT fk_criador_post FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.usuario_da_like_em_post (
+	usuario_username VARCHAR(12),
+	post_id INT,
+	CONSTRAINT pk_usuario_da_like_em_post PRIMARY KEY(usuario_username, post_id),
+	CONSTRAINT fk_usuario_like_post FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_post_liked_usuario FOREIGN KEY(post_id)
+		REFERENCES rede_musical.post(id)
+);
+
+CREATE TABLE rede_musical.usuario_compartilha_post (
+	usuario_username VARCHAR(12),
+	post_id INT,
+	CONSTRAINT pk_usuario_compartilha_post PRIMARY KEY(usuario_username, post_id),
+	CONSTRAINT fk_usuario_compartilhou FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_post_compartilhado FOREIGN KEY(post_id)
+		REFERENCES rede_musical.post(id)
+);
+
+CREATE TABLE rede_musical.comentario (
+	id SERIAL,
+	dt_publicacao TIMESTAMP,
+	texto_comentario TEXT,
+	CONSTRAINT pk_comentario PRIMARY KEY(id)
+);
+
+CREATE TABLE rede_musical.usuario_comenta_post (
+	usuario_username VARCHAR(12) NOT NULL,
+	post_id INT,
+	comentario_id INT,
+	CONSTRAINT pk_usuario_comenta_post PRIMARY KEY(usuario_username),
+	CONSTRAINT fk_usuario_autor FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_post_comentado FOREIGN KEY(post_id)
+		REFERENCES rede_musical.post(id),
+	CONSTRAINT fk_comentario_feito FOREIGN KEY(comentario_id)
+		REFERENCES rede_musical.comentario(id)
+);
+
+CREATE TABLE rede_musical.feed (
+	usuario_username VARCHAR(12),
+	CONSTRAINT pk_feed PRIMARY KEY(usuario_username),
+	CONSTRAINT fk_feed_usuario FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.feed_possui_posts (
+	feed_username VARCHAR(12),
+	post_id INT,
+	CONSTRAINT pk_feed_possui_posts PRIMARY KEY(feed_username, post_id),
+	CONSTRAINT fk_feed_post FOREIGN KEY(feed_username)
+		REFERENCES rede_musical.feed(usuario_username),
+	CONSTRAINT fk_post_feed FOREIGN KEY(post_id)
+		REFERENCES rede_musical.post(id)
+);
+
+CREATE TABLE rede_musical.banda  (
+	sigla VARCHAR(5),
+	nome VARCHAR(30),
+	imagem TEXT,
+	genero_musical VARCHAR(20),
+	usuario_username VARCHAR(12) NOT NULL,
+	CONSTRAINT pk_banda PRIMARY KEY(sigla),
+	CONSTRAINT fk_usuario_criador_banda FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.usuario_segue_banda (
+	usuario_username VARCHAR(12),
+	banda_sigla VARCHAR(5),
+	CONSTRAINT pk_usuario_segue_banda PRIMARY KEY(usuario_username, banda_sigla),
+	CONSTRAINT fk_usuario_segue_banda FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_banda_seguida_usuario FOREIGN KEY(banda_sigla)
+		REFERENCES rede_musical.banda(sigla)
+);
+
+CREATE TABLE rede_musical.usuario_participa_de_banda (
+	usuario_username VARCHAR(12),
+	banda_sigla VARCHAR(5),
+	instrumento VARCHAR(20),
+	CONSTRAINT pk_usuario_participa_banda PRIMARY KEY(usuario_username, banda_sigla),
+	CONSTRAINT fk_usuario_participa_banda FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_banda_participada_usuario FOREIGN KEY(banda_sigla)
+		REFERENCES rede_musical.banda(sigla)
+);
+
+CREATE TABLE rede_musical.post_banda (
+	post_id INT,
+	banda_sigla VARCHAR(5) NOT NULL,
+	CONSTRAINT pk_post_banda PRIMARY KEY(post_id),
+	CONSTRAINT fk_post_banda FOREIGN KEY(post_id)
+		REFERENCES rede_musical.post(id),
+	CONSTRAINT fk_banda_post FOREIGN KEY(banda_sigla)
+		REFERENCES rede_musical.banda(sigla)
+);
+
+CREATE TABLE rede_musical.evento (
+	id SERIAL,
+	data TIMESTAMP,
+	nome VARCHAR(30),
+	descricao TEXT,
+	numero_participantes INT,
+	categoria VARCHAR(20),
+	usuario_username VARCHAR(12) NOT NULL,
+	CONSTRAINT pk_evento PRIMARY KEY(id),
+	CONSTRAINT fk_usuario_criador_evento FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username)
+);
+
+CREATE TABLE rede_musical.usuario_comparece_em_evento (
+	usuario_username VARCHAR(12),
+	evento_id INT,
+	CONSTRAINT pk_usuario_comparece_evento PRIMARY KEY(usuario_username, evento_id),
+	CONSTRAINT fk_usuario_comparece_evento FOREIGN KEY(usuario_username)
+		REFERENCES rede_musical.usuario(username),
+	CONSTRAINT fk_evento_comparecido_usuario FOREIGN KEY(evento_id)
+		REFERENCES rede_musical.evento(id)
+);
