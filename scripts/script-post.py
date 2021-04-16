5ec132afc6a97bd8e690dbab1edd39a6ad17add6
reisclaudio <claudioneto75@gmail.com>
2021-04-16 00:09:43 -0300

    Adicionando script para aleatorizar posts

--- scripts/script-post.py
+++ scripts/script-post.py
@@ -12,6 +12,8 @@ try:
 
     query = """ INSERT INTO rede_musical.post (ID, TEXTO_POST, DT_PUBLICACAO, USUARIO_ID) VALUES (%s,%s,%s,%s)"""
     query_feed = """ INSERT INTO rede_musical.feed_possui_posts (FEED_ID, POST_ID, ID_USUARIO_COMPARTILHOU) VALUES (%s,%s, -1)"""
+    query_seguidores = """ SELECT usuario_id FROM rede_musical.usuario_segue_usuario WHERE usuario_id_seguido = %s """
+
     while (i < 99):
         id_usuario = random.randint(1, 98)
 
@@ -29,7 +31,24 @@ try:
 
         cursor.execute(query, insert)
         cursor.execute(query_feed, insert_feed)
-        i+=1
+        cursor.execute(query_seguidores, [id_usuario])
+        
+        row = cursor.fetchone()
+
+        lista_seguidores = []
+        while row is not None:
+            lista_seguidores.append(row[0])
+            row = cursor.fetchone()
+
+        for j in lista_seguidores:
+            insert_feed =(
+                j,
+                i
+            )
+            
+            cursor.execute(query_feed, insert_feed)
+        
+        i+=1 
 
 
     connection.commit()
