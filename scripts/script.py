0682600c50b49336975c4e73e21addcb0dff4639
Vinicius <vinicius.cesar@uel.br>
2021-04-17 16:06:17 -0300

    Implementado script gerador de likes em posts.

--- scripts/script.py
+++ scripts/script.py
@@ -2,6 +2,7 @@ import psycopg2
 from delete_script import limpar_tabelas
 from script_usuario import gerar_usuarios
 from script_post import gerar_posts
+from script_post_like import gerar_likes
 
 connection = psycopg2.connect(user="postgres",
                                     password="8468",
@@ -17,6 +18,8 @@ try:
     gerar_usuarios(connection, cursor)
 
     gerar_posts(connection, cursor)
+
+    gerar_likes(connection, cursor)
 
 except (Exception, psycopg2.Error) as error:
         print("Falha ao inserir", error)
--- scripts/script_post_like.py
+++ scripts/script_post_like.py
@@ -2,5 +2,26 @@ import psycopg2
 import random
 
 def gerar_likes(connection, cursor):
+    buscar_posts_query = """SELECT post_id FROM rede_musical.feed_possui_posts fpp WHERE feed_id = %s"""
 
-    for i in range(99):
+    inserir_like_query = """INSERT INTO rede_musical.usuario_da_like_em_post (usuario_id , post_id) VALUES (%s, %s)"""
+
+    for i in range(100):
+        cursor.execute(buscar_posts_query, [i])
+        row = cursor.fetchone()
+
+        lista_posts = []
+        while row is not None:
+            lista_posts.append(row[0])
+            row = cursor.fetchone()
+
+        total_likes = random.randint(0, len(lista_posts))
+
+        for j in range(total_likes):
+            post_like = random.randint(0, len(lista_posts) - 1)
+
+            cursor.execute(inserir_like_query, (i, lista_posts.pop(post_like)))
+
+    connection.commit()
+    count = cursor.rowcount 
+    print(count, "Inserção de Likes finalizada")
