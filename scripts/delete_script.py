2a5bc768663882836ff0a958dc86dfa4749badb1
reisclaudio <claudioneto75@gmail.com>
2021-04-19 19:52:39 -0300

    Ajustes script

--- scripts/delete_script.py
+++ scripts/delete_script.py
@@ -1,26 +1,18 @@
 import psycopg2
 
 def limpar_tabelas(connection, cursor):
-    limpar_usuarios = """DELETE FROM rede_musical.usuario"""
-    limpar_usuario_segue_usuario = """DELETE FROM rede_musical.usuario_segue_usuario"""
-    limpar_instrumentos_usuario = """DELETE FROM rede_musical.usuario_instrumentos"""
-    limpar_posts = """DELETE FROM rede_musical.post"""
-    limpar_feed = """DELETE FROM rede_musical.feed"""
-    limpar_feed_possui_posts = """DELETE FROM rede_musical.feed_possui_posts"""
-    limpar_bandas = """DELETE FROM rede_musical.banda"""
-    limpar_eventos = """DELETE FROM rede_musical.evento"""
-    limpar_usuario_segue_banda = """DELETE FROM rede_musical.usuario_segue_banda"""
+    limpar_usuarios = """TRUNCATE TABLE rede_musical.usuario RESTART IDENTITY CASCADE"""
+    limpar_posts = """TRUNCATE TABLE rede_musical.post RESTART IDENTITY CASCADE"""
+    limpar_feed = """TRUNCATE TABLE rede_musical.feed RESTART IDENTITY CASCADE"""
+    limpar_bandas = """TRUNCATE TABLE rede_musical.banda RESTART IDENTITY CASCADE"""
+    limpar_eventos = """TRUNCATE TABLE rede_musical.evento RESTART IDENTITY CASCADE"""
     gerar_usuario = """INSERT INTO rede_musical.usuario VALUES(-1, 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'M', '2000-04-18', 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'nenhum', 'nenhum')"""
 
     cursor.execute(limpar_usuarios)
     cursor.execute(limpar_bandas)
     cursor.execute(limpar_eventos)
-    cursor.execute(limpar_instrumentos_usuario)
     cursor.execute(gerar_usuario)
-    cursor.execute(limpar_usuario_segue_usuario)
-    cursor.execute(limpar_usuario_segue_banda)
     cursor.execute(limpar_posts)
     cursor.execute(limpar_feed)
-    cursor.execute(limpar_feed_possui_posts)
 
     connection.commit()
